c2beba2efcfcfdd32fd226cda99827d507bfef1f
alaa old Desktop <alaa19090@gmail.com>
2022-02-26 22:56:09 +0300

    finsh rating

--- components/Content.vue
+++ components/Content.vue
@@ -115,7 +115,9 @@
       <v-card class="rounded-lg py-8 px-6 mb-10" max-width="100%">
         <v-card-title class="pa-0">
           <h2 class="font-weight-bold text-h5 accent--text mb-4">
-            <v-icon class="mb-2" color="primary" size="30">mdi-medical-bag</v-icon>
+            <v-icon class="mb-2" color="primary" size="30"
+              >mdi-medical-bag</v-icon
+            >
             <span class="mx-1">{{ $t('DoctorPage.Workplace') }}</span>
           </h2>
         </v-card-title>
@@ -263,11 +265,50 @@
           </span>
         </v-card-text>
       </v-card>
-
     </div>
 
     <!-- Rating section -->
-    <div class="rating-doctor" v-else></div>
+    <div class="rating-doctor" v-else>
+      <v-card class="rounded-lg py-8 px-6 mb-10" max-width="100%">
+        <v-card-title class="pa-0">
+          <h2 class="font-weight-bold text-h5 accent--text mb-3">
+            <v-icon color="primary" size="30">mdi-comment-multiple-outline</v-icon>
+            <span class="mx-1">{{ $t('DoctorPage.rating') }}</span>
+          </h2>
+        </v-card-title>
+        <v-list three-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              v-text="item.header"
+            ></v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-item v-else :key="item.title">
+              <v-list-item-avatar>
+                <v-img :src="item.avatar"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-html="item.subtitle"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-rating :value="5" color="amber" dense readonly></v-rating>
+              </v-list-item-action>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-card>
+    </div>
   </section>
 </template>
 <script>
@@ -275,6 +316,41 @@ export default {
   data() {
     return {
       toggle_exclusive: 0, //on select button of button group is return the index of button
+      items: [
+        { header: 'Today' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle:
+            '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle:
+            '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle:
+            '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
     }
   },
 }
@@ -293,8 +369,7 @@ export default {
   span {
     width: 48%;
     @include tablet {
-      width:100%;
-
+      width: 100%;
     }
   }
 }
