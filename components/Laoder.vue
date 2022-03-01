e7fddf645a7c43d47b897d2c3e2a976140e3b4f7
alaa old Desktop <alaa19090@gmail.com>
2022-03-01 14:50:09 +0300

    better loader

--- components/Laoder.vue
+++ components/Laoder.vue
@@ -1,15 +1,22 @@
 <template>
   <v-overlay color="white" :value="showHideSpinner" opacity="0.95">
-    <v-progress-circular indeterminate color="primary" size="64"></v-progress-circular>
+    <v-progress-circular
+      indeterminate
+      color="primary"
+      size="64"
+    ></v-progress-circular>
   </v-overlay>
 </template>
 <script>
 export default {
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => (this.showHideSpinner = false), 500)
+    })
+  },
+
   beforeCreate() {
     this.showHideSpinner = true
-  },
-  mounted() {
-    this.showHideSpinner = false
   },
   data() {
     return {
